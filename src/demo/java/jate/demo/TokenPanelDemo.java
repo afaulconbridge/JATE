689000c02e3820c63fa60227ff2e5eab6520a402
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-01 19:39:31 +0000

    Added token panel and demo

---
+++ src/demo/java/jate/demo/TokenPanelDemo.java
@@ -0,0 +1,44 @@
+package jate.demo;
+
+import javax.swing.JFrame;
+
+import jate.TilePanel;
+import jate.TokenPanel;
+import jate.map.TileMap;
+import jate.map.TileNestedHashMap;
+import jate.map.TokenHashMap;
+import jate.map.TokenMap;
+import jate.tile.FileTile;
+import jate.tile.Tile;
+import jate.token.FileToken;
+import jate.token.Token;
+
+public class TokenPanelDemo {
+
+	public TokenPanelDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(640, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Token token = new FileToken("tile_001568");
+		
+		TokenMap tokenMap = new TokenHashMap(32,32);
+		for (int x = 0; x < 100; x++) {
+			for (int y = 0; y < 100; y++) {
+				tokenMap.add(x, y, token);
+			}
+		}
+		
+		TokenPanel panel = new TokenPanel(tokenMap);
+		
+		frame.add(panel);
+		
+		frame.setVisible(true);
+		
+	}
+
+}
---
+++ src/main/java/jate/TokenPanel.java
@@ -0,0 +1,45 @@
+package jate;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jate.map.TokenMap;
+import jate.render.TokenRenderer;
+
+public class TokenPanel extends JPanel {
+
+	private static final long serialVersionUID = -419684051431254556L;
+
+	protected TokenMap tokenMap = null;
+	
+    private Logger log = LoggerFactory.getLogger(getClass());
+	
+	public TokenPanel(TokenMap tokenMap) {
+		super();
+
+		//mark as transparent
+        this.setOpaque(false);
+        
+		this.tokenMap = tokenMap;
+	}
+
+	public TokenMap getTokenMap() {
+		return tokenMap;
+	}
+
+	@Override
+    protected void paintComponent(Graphics g) {
+    	Graphics2D gg = (Graphics2D) g;
+    	
+    	if (tokenMap == null) {
+    		return;
+    	}
+    	
+    	TokenRenderer.render(gg, tokenMap, 0,0, getWidth(),getHeight());
+	}
+	
+}
