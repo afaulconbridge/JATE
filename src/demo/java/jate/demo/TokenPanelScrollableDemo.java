1c7bdfeb9ba979a5f23f1f22327846994989817e
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-01 22:03:36 +0000

    Added token scrollable panel and demo

---
+++ src/demo/java/jate/demo/TokenPanelScrollableDemo.java
@@ -0,0 +1,45 @@
+package jate.demo;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+import jate.TokenPanelScrollable;
+import jate.map.TokenHashMap;
+import jate.map.TokenMap;
+import jate.token.FileToken;
+import jate.token.Token;
+
+public class TokenPanelScrollableDemo {
+
+	public TokenPanelScrollableDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.setSize(640, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Token token = new FileToken("tile_001568");
+		
+		TokenMap tokenMap = new TokenHashMap(100,100, 32,32);
+		for (int x = 0; x < 100; x++) {
+			for (int y = 0; y < 100; y++) {
+				tokenMap.add(x, y, token);
+			}
+		}
+		
+		
+		TokenPanelScrollable tokenPanel = new TokenPanelScrollable(tokenMap);	
+		JScrollPane scrollPane = new JScrollPane(tokenPanel);
+		
+		frame.add(scrollPane, BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+		
+	}
+
+}
---
+++ src/main/java/jate/TokenPanelScrollable.java
@@ -0,0 +1,84 @@
+package jate;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import javax.swing.Scrollable;
+import javax.swing.SwingConstants;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jate.map.TileMap;
+import jate.map.TokenMap;
+import jate.render.TileRenderer;
+import jate.render.TokenRenderer;
+
+public class TokenPanelScrollable extends TokenPanel implements Scrollable {
+
+	private static final long serialVersionUID = 8495111616006504813L;
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+	
+	public TokenPanelScrollable(TokenMap tokenMap) {
+		super(tokenMap);
+		setPreferredSize(new Dimension((int)tokenMap.getWidth()*tokenMap.getUnitWidth(),
+				(int)tokenMap.getHeight()*tokenMap.getUnitHeight()));
+	}
+
+	@Override
+	public Dimension getPreferredScrollableViewportSize() {
+		return getPreferredSize();
+	}
+
+
+	@Override
+	public boolean getScrollableTracksViewportHeight() {
+		return false;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportWidth() {
+		return false;
+	}
+	
+	@Override
+	public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+		if (orientation == SwingConstants.VERTICAL) {
+			if (direction < 0) {
+				//going up
+				return (int) (visibleRect.getHeight()/2);
+			} else {
+				//assume down
+				return (int) (visibleRect.getHeight()/2);
+			}
+		} else {
+			//assume horizontal
+			if (direction < 0) {
+				//going left
+				return (int) (visibleRect.getWidth()/2);
+			} else {
+				//assume right
+				return (int) (visibleRect.getWidth()/2);
+			}
+		}
+	}
+
+	@Override
+	public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+		return 1;
+	}
+	
+	@Override
+    protected void paintComponent(Graphics g) {
+    	Graphics2D gg = (Graphics2D) g;
+    	
+    	if (tokenMap == null) {
+    		return;
+    	}
+    	Rectangle r = getVisibleRect();
+    	TokenRenderer.render(gg, tokenMap, r.x, r.y , r.x+r.width, r.y+r.height);
+	}
+
+}
